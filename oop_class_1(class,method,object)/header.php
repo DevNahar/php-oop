9d3a714e98c247090ab8f00c3cfbd2476f01b5a9
DevNahar <ashekunnahar@gmail.com>
2022-08-12 01:48:57 +0600

    class,method,object

---
+++ oop_class_1(class,method,object)/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Oop</title>
+  </head>
+  <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="card mt-5 ">
+                        <div class="card-header bg-primary p-3  "><h1 class="text-white text-center">Object Oriented PHP</h1></div>
+                        <div class="card-body p-3 " style="border: 5px solid #007bff">
---
+++ oop_class_1(class,method,object)/index.php
@@ -0,0 +1,44 @@
+<?php  include "header.php";?>
+
+                        
+<!--======== 1st class code =========-->
+
+<h2 class="text-center">OOP First Class</h2>
+
+<?php
+
+class Person{
+    public $name = "Ashekun nahar";  //dui babe kora jai
+    // public $name ;     //dui babe kora jai
+    public $age;
+
+    function personName(){
+      echo "Person name is : ". $this->name ;
+    }
+
+    function personAge($value){
+      return "Person age is : " . $this->age = $value; // $value k $age poperty e re-assign kora hoese
+      
+    }
+  }
+  
+// object create
+$personObj = new Person;
+//poperty show
+echo $personObj->name ;   //dui babe kora jai
+// echo $personObj->name = "Ashekun Nahar";  //ababe o assign kora jai
+echo "<br>";
+//method show
+$personObj->personName() ; //method e echo korle ekhane echo kora lagbe na
+echo "<br>";
+echo $personObj->personAge(18) ; // ebabe argument pathano jai
+
+
+?>
+                      
+                      
+<?php  include "footer.php";?>                   
+                      
+                      
+                      
+                      
